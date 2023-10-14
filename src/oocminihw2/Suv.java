28f84185925335e82d0d57d8a1f2fca1b9003dd1
2022395 <2022395@student.cct.ie>
2023-10-14 19:46:36 +0100

    Initial commit

---
+++ src/oocminihw2/Suv.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package oocminihw2;
+
+/**
+ *
+ * @author volkan
+ */
+public class Suv extends Vehicle {
+    
+    protected boolean isFourWheelDriveOn=true;
+    protected double suspensionHeight;
+    
+}
